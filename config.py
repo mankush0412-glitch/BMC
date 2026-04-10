623e5bc638cc026c57b2203b70cac72a57bb1a15
mankush0412-glitch <mankush0412@gmail.com>
2026-04-10 17:37:43 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,30 @@
+# devgagan
+# Note if you are trying to deploy on vps then directly fill values in ("")
+
+from os import getenv
+
+# VPS --- FILL COOKIES 🍪 in """ ... """ 
+
+INST_COOKIES = """
+	# write here cookies 
+"""
+
+YTUB_COOKIES = """
+# write here coockies 
+"""
+
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+MONGO_DB = getenv("MONGO_DB", "")
+LOG_GROUP = getenv("LOG_GROUP", "")
+CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
+PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
+WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
+AD_API = getenv("AD_API", "446b3a3f0039a2826f1483f22e9080963974ad3b")
+STRING = getenv("STRING", None)
+YT_COOKIES = getenv("YT_COOKIES", "")
+DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
+INSTA_COOKIES = getenv("INSTA_COOKIES", "")
